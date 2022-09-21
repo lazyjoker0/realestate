c90fdc123ff4daecfae54907a98343238af79530
lazy_joker0 <73578873+lazyjoker0@users.noreply.github.com>
2022-09-21 23:39:49 +0500

    next.config file

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  customWorkerDir: 'serviceworker',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: true,
+  swcMinify: true,
+})
